a9b0c3c9edbbd99abb832fa9c827fd74804d1f06
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2021-04-20 17:38:13 +0200

    Document Timezone field as "IANA timezone"

--- bemserver/app/api/__init__.py
+++ bemserver/app/api/__init__.py
@@ -1,9 +1,11 @@
 """BEMServer API"""
 from .extensions import Api, Blueprint, Schema, AutoSchema, SQLCursorPage  # noqa
+from .extensions.ma_fields import Timezone
 from .resources import register_blueprints
 
 
 def init_app(app):
     api = Api()
     api.init_app(app)
+    api.register_field(Timezone, 'string', 'IANA timezone')
     register_blueprints(api)
